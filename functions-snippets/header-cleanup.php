929c7c7ef0a4ee121cbe52bfd81c4e82c384e903
tgarries <taylor@taylorgarries.com>
2021-01-12 09:53:05 -0700

    Update and rename header-cleanup to header-cleanup.php

--- functions-snippets/header-cleanup.php
+++ functions-snippets/header-cleanup.php
@@ -42,5 +42,4 @@ remove_action('admin_print_scripts', 'print_emoji_detection_script' );
 remove_action('wp_print_styles', 'print_emoji_styles' );
 remove_action('admin_print_styles', 'print_emoji_styles' );  
 
-
 ?>
