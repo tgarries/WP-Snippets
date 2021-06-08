4b11bad7de89ddfce333721ebc5251a0a5f34c5a
tgarries <taylor@taylorgarries.com>
2021-06-08 10:59:50 -0600

    Update login-branding.php

--- functions-snippets/login-branding.php
+++ functions-snippets/login-branding.php
@@ -31,3 +31,25 @@ add_action( 'login_head', 'namespace_login_style' );
 function namespace_login_style() {
     echo '<style>.login h1 a { background-image: url( ' . get_template_directory_uri() . '/images/logo.png ) !important; }</style>';
 }
+
+
+/*
+    Use Site Logo for login page
+*/
+function namespace_login_style() {
+    if( function_exists('get_custom_header') ){
+        $width = get_custom_header()->width;
+        $height = get_custom_header()->height;
+    } else {
+        $width = HEADER_IMAGE_WIDTH;
+        $height = HEADER_IMAGE_HEIGHT;
+    }
+    echo '<style>'.PHP_EOL;
+    echo '.login h1 a {'.PHP_EOL; 
+    echo '  background-image: url( '; header_image(); echo ' ) !important; '.PHP_EOL;
+    echo '  width: '.$width.'px !important;'.PHP_EOL;
+    echo '  height: '.$height.'px !important;'.PHP_EOL;
+    echo '  background-size: '.$width.'px '.$height.'px !important;'.PHP_EOL;
+    echo '}'.PHP_EOL;
+    echo '</style>'.PHP_EOL;
+}
