869a45455d9eb069c548d14a0642a7cee2412f3a
tgarries <taylor@taylorgarries.com>
2022-09-19 18:27:41 -0600

    Update 01.php

--- functions-snippets/01.php
+++ functions-snippets/01.php
@@ -28,6 +28,9 @@ add_filter('the_generator', '__return_empty_string');
 
 // remove version from scripts and styles
 function tg_remove_version_scripts_styles($src) {
+
+	if( is_admin() || wp_doing_ajax() ) return $src;
+	
 	if (strpos($src, 'ver=')) {
 		$src = remove_query_arg('ver', $src);
 	}
